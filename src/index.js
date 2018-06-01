bf0903bbca241d6b81089207ef7dd7704a5ee8fa
Nikita Skorodumov <mr-zerx@yandex.ru>
2018-06-01 22:12:49 +0500

    Заменил сборку и продолжил вёрстку

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import './style/style.scss';
+import img from './img/logo.png';
+import img1 from './img/gray.png';
+import img2 from './img/plus.png';
+import img3 from './img/search.png';
+import img4 from './img/white.png';
+
+
+
+
+
+// let elem = document.querySelector('#but');
+// let reuslt = document.querySelector('.list');
+
+// elem.addEventListener('click', ()=> {
+//   let row = document.createElement('TR');
+
+//   row.className='row';
+//   row.innerHTML = '<td>Раз</td><td>Два</td><td><button>Удалить</button></td>';
+//   reuslt.appendChild(row); 
+// });
